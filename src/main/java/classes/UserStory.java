219c4ee4e7eea90318fe847a43f0c81d638c0d23
ReesMehorst <24141720@student.hhs.nl>
2025-03-26 11:46:14 +0100

    Epic linker UserStory

---
+++ src/main/java/classes/UserStory.java
@@ -0,0 +1,43 @@
+package classes;
+
+import java.util.ArrayList ;
+public class UserStory implements Linker {
+    private String usName ;
+    private String usDescription ;
+    private ArrayList<Task>  tasks ;
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setUsName(String usName) {
+        this.usName = usName;
+    }
+
+    public String getUsName() {
+        return this.usName;
+    }
+
+    public void setUsDescription(String usDescription) {
+        this.usDescription = usDescription;
+    }
+
+    public String getUsDescription() {
+        return this.usDescription;
+    }
+
+    public UserStory(String usName, String usDescription) {
+        this.usName = usName;
+        this.usDescription = usDescription;
+        this.tasks = new ArrayList<Task>();
+    }
+
+    @Override
+    public void linkMessage() {
+
+    }
+}
