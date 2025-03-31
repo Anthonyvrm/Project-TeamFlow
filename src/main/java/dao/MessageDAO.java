924d2df3462ee3b2cf417555e5c51880e20379ba
ReesMehorst <24141720@student.hhs.nl>
2025-03-31 11:28:50 +0200

    Nieuwe branch, en MessageDAO aangemaakt

---
+++ src/main/java/dao/MessageDAO.java
@@ -0,0 +1,32 @@
+package dao;
+import classes.Chat;
+import classes.User;
+
+import database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MessageDAO {
+    public static void insertMessage(User user, String message, boolean isHighlighted, Chat chat) {
+        String sql = "INSERT INTO Message(userID ,message, isHighlighted, chatID) VALUES(?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, user.getUserID());
+            pstmt.setString(2, message);
+            pstmt.setBoolean(3, isHighlighted);
+            pstmt.setInt(4, chat.getChatID());
+            pstmt.executeUpdate();
+            System.out.println("User inserted successfully.");
+        } catch (SQLException e) {
+            System.out.println("Insert failed: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
