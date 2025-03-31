e9a3bc017a632275136ce02e3ac66bb71a9109f5
ReesMehorst <24141720@student.hhs.nl>
2025-03-31 11:06:38 +0200

    User aangepast naar Users, het is namelijk een SQL gereserveerd woord.

--- src/main/java/classes/UserStory.java
+++ src/main/java/classes/UserStory.java
@@ -4,18 +4,19 @@ import java.util.ArrayList ;
 public class UserStory implements Linker {
     private String usName ;
     private String usDescription ;
+    private int usID ;
     private ArrayList<Task>  tasks ;
 
-    public UserStory(String usName, String usDescription) {
+    public UserStory(String usName, String usDescription, int usID) {
         this.usName = usName;
         this.usDescription = usDescription;
+        this.usID = usID;
         this.tasks = new ArrayList<>();
     }
 
     public void setTasks(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
-
     public ArrayList<Task> getTasks() {
         return this.tasks;
     }
@@ -23,7 +24,6 @@ public class UserStory implements Linker {
     public void setUsName(String usName) {
         this.usName = usName;
     }
-
     public String getUsName() {
         return this.usName;
     }
@@ -31,9 +31,15 @@ public class UserStory implements Linker {
     public void setUsDescription(String usDescription) {
         this.usDescription = usDescription;
     }
-
     public String getUsDescription() {
         return this.usDescription;
+    }
+
+    public void setUsID(int usID) {
+        this.usID = usID;
+    }
+    public int getUsID() {
+        return this.usID;
     }
 
     @Override
