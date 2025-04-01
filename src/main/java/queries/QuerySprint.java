6b5f9fc0d0566109486cae19d3af996ed29a2dd7
RobbertSchwarze <robbertschwarze2309@gmail.com>
2025-04-01 12:10:10 +0200

    Query Sprint

---
+++ src/main/java/queries/QuerySprint.java
@@ -0,0 +1,32 @@
+package queries;
+
+import database.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class QuerySprint {
+    public static void getSprintQuery() {
+        String sql = "SELECT sprintID, sprintInt FROM Sprint";
+
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                int sprintID = rs.getInt("sprintID");
+                int sprintInt = rs.getInt("sprintInt");
+
+                System.out.println("sprintID: " + sprintID + ", sprintInt: " + sprintInt);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Query mislukt: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        getSprintQuery();
+    }
+}
