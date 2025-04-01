16b12bca87b3d1ff5fb70dbcb62f29f1da94e8bb
mirtanov <156001991+mirtanov@users.noreply.github.com>
2025-04-01 12:30:24 +0200

    dao

--- src/main/java/dao/SprintDAO.java
+++ src/main/java/dao/SprintDAO.java
@@ -1,5 +1,32 @@
 package dao;
 
-public class SprintDAO {
+import database.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class SprintDAO {
+    public static void insertSprint(int sprintInt, int sprintID, LocalDateTime startDate, LocalDateTime endDate) {
+        String sql = "INSERT INTO Sprint(sprintInt, startDate, endDate) VALUES(?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, sprintInt);
+            pstmt.setTimestamp(3, java.sql.Timestamp.valueOf(startDate));
+            pstmt.setTimestamp(4, java.sql.Timestamp.valueOf(endDate));
+            pstmt.executeUpdate();
+            System.out.println("Sprint inserted successfully.");
+        } catch (SQLException e) {
+            System.out.println("Insert failed: " + e.getMessage());
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
 }
