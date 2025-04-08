643792ae8ae4236e1d8f986902673fa8a11475dd
ReesMehorst <24141720@student.hhs.nl>
2025-04-08 12:34:17 +0200

    SprintDAO TaskDAO & DatabaseSetup fix

--- src/main/java/dao/SprintDAO.java
+++ src/main/java/dao/SprintDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import classes.*;
 import database.DatabaseConnection;
 
 import java.sql.*;
@@ -7,37 +8,26 @@ import java.time.LocalDateTime;
 
 public class SprintDAO {
 
-    public static int insertSprintAndReturnID(int sprintInt, LocalDateTime startDate, LocalDateTime endDate) {
+    public static int insertSprintAndReturnID(Sprint sprint) {
         String sql = "INSERT INTO Sprint(sprintInt, startDate, endDate) VALUES(?, ?, ?)";
-        int sprintID = -1 ;
+        int sprintID = -1;
         try (Connection conn = DatabaseConnection.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            pstmt.setInt(1, sprintInt);
-            pstmt.setTimestamp(2, java.sql.Timestamp.valueOf(startDate));
-            pstmt.setTimestamp(3, java.sql.Timestamp.valueOf(endDate));
+            pstmt.setInt(1, sprint.getSprintInt());
+            pstmt.setTimestamp(2, java.sql.Timestamp.valueOf(sprint.getStartDate()));
+            pstmt.setTimestamp(3, java.sql.Timestamp.valueOf(sprint.getEndDate()));
             pstmt.executeUpdate();
 
             try (ResultSet keys = pstmt.getGeneratedKeys()) {
                 if (keys.next()) {
                     sprintID = keys.getInt(1);
-                    System.out.println("Sprint created: " + sprintInt + " (ID: " + sprintID + ")");
+                    System.out.println("Sprint created: " + sprint.getSprintInt() + " (ID: " + sprintID + ")");
                 }
             }
             System.out.println("Sprint inserted successfully.");
         } catch (SQLException e) {
             System.out.println("Insert failed: " + e.getMessage());
         }
-        return sprintID ;
+        return sprintID;
     }
-
-    //public static void insertTestSprint() {
-        //int sprintNumber = 1;
-       // LocalDateTime startDate = LocalDateTime.now();
-        //LocalDateTime endDate = startDate.plusWeeks(2);
-
-        //insertSprint(sprintNumber, startDate, endDate);
-
-
-
-    }
-//}
+}
