9f363616737f15a556520d099f35708717be8348
ReesMehorst <24141720@student.hhs.nl>
2025-04-11 12:05:01 +0200

    UserStoryDAO aangepast, zodat de chatID ook meegegeven wordt

--- src/main/java/dao/UserstoryDAO.java
+++ src/main/java/dao/UserstoryDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import classes.*;
 import database.DatabaseConnection;
+import queries.QueryChats;
 import queries.QueryEpics;
 
 import java.sql.Connection;
@@ -10,13 +11,14 @@ import java.sql.SQLException;
 
 public class UserstoryDAO {
     public static void insertUserstory(UserStory userstory) {
-        String sql = "INSERT INTO Userstory (usName, usDescription, epicID) VALUES(?,?,?)";
+        String sql = "INSERT INTO Userstory (usName, usDescription, epicID, chatID) VALUES(?,?,?,?)";
 
         try (Connection conn = DatabaseConnection.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, userstory.getUsName());
             pstmt.setString(2, userstory.getUsDescription());
             pstmt.setInt(3, QueryEpics.getEpicID(userstory.getEpic()));
+            pstmt.setInt(4, QueryChats.getChatID(userstory.getUserStoryChat()));
 
             pstmt.executeUpdate();
             System.out.println("Userstory inserted successfully.");
